ae536e23f2ec603fb71989ab3a14951311b5d854
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2023-01-20 21:54:41 -0300

    feat: creating empty todos layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ClipboardText } from 'phosphor-react';
+
 import { Header } from './components/Header'
 import { TodoForm } from './components/TodoForm'
 
@@ -13,8 +15,21 @@ export function App() {
             <div className={styles.mainContent}>
                 <div className={styles.createdAndFinishedTodos}>
                     <span className={styles.createdTasks}>
-                        Tarefas criadas 0</span>
-                    <span className={styles.finishedTasks}>Concluídas 0</span>
+                        Tarefas criadas <span className={styles.counter}>0</span>
+                    </span>
+                    <span className={styles.finishedTasks}>
+                        Concluídas <span className={styles.counter}>0</span>
+                    </span>
+                </div>
+
+                <div className={styles.todos}>
+                    <div className={styles.empty}>
+                       <ClipboardText size={56} />
+                       <div className={styles.emptyText}>
+                           <p> Você ainda não tem tarefas cadastradas </p>
+                           <p> Crie tarefas e organize seus itens a fazer </p>
+                       </div>
+                    </div>
                 </div>
             </div>
         </div>
