2c50d4b777e561776876c14d70f6cc8fbcc13d72
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2023-01-23 08:40:35 -0300

    feat: invalidation on create a todo without title

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,5 +1,5 @@
 import { PlusCircle } from "phosphor-react";
-import { FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
 
 import styles from "./TodoForm.module.css"
 
@@ -10,15 +10,31 @@ interface TodoFormProps {
 export function TodoForm({ onCreateTodo }: TodoFormProps) {
     const [text, setText] = useState('');
 
+    function handleNewText(event: ChangeEvent<HTMLInputElement>) {
+        event.target.setCustomValidity('');
+        setText(event.target.value);
+
+    }
+
     function handleSubmitForm(event: FormEvent) {
         event.preventDefault();
         onCreateTodo(text);
         setText('');
     }
 
+    function handleInputWithError(event: InvalidEvent<HTMLInputElement>) {
+        event.target.setCustomValidity('Este campo é obrigatório!');
+    }
+
     return (
         <form onSubmit={handleSubmitForm} className={styles.form}>
-            <input onChange={(event) => setText(event.target.value)} placeholder="Adicione uma nova tarefa" type="text" />
+            <input
+                required
+                onChange={handleNewText}
+                onInvalid={handleInputWithError}
+                placeholder="Adicione uma nova tarefa"
+                type="text"
+            />
             <button>Criar <PlusCircle size={16} /></button>
         </form>
     )
